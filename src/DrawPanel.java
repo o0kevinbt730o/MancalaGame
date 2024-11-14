e03623f90f932fda0725f5561f86da8b24dd1d52
KevinChau <hoang.chau@sjsu.edu>
2024-11-13 23:31:37 -0800

    Day 3 - Worked on the MancalaModel, MancalaView, and initial screen

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,9 +1,6 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.geom.*;
-
-import javax.swing.*;
+import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class DrawPanel extends JPanel {
@@ -12,6 +9,7 @@ public class DrawPanel extends JPanel {
 	private int boardWidth = 1025;
 	private int boardHeight = 350;
 	private Graphics2D g2;
+	private ArrayList<PrintPitWithStones> allPits = new ArrayList<>();
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -34,14 +32,30 @@ public class DrawPanel extends JPanel {
 		PrintPitWithStones a5 = new PrintPitWithStones(700, 275, 100);
 		PrintPitWithStones a6 = new PrintPitWithStones(825, 275, 100);
 		
+		allPits.add(b6);
+		allPits.add(b5);
+		allPits.add(b4);
+		allPits.add(b3);
+		allPits.add(b2);
+		allPits.add(b1);
+		
+		allPits.add(a1);
+		allPits.add(a2);
+		allPits.add(a3);
+		allPits.add(a4);
+		allPits.add(a5);
+		allPits.add(a6);
+		
+		
+		
 		
 		PrintMancalaWithStones mancalaB = new PrintMancalaWithStones(75, 75, 100, 300);
 		PrintMancalaWithStones mancalaA = new PrintMancalaWithStones(955, 75, 100, 300);
 		
-//		b2.removeStone4(); //makes b2 disappear a stone
+//		b2.removeStone4(); //makes a stone in b2 disappear
 		
 
-	}
+	} //end of paintComponent()
 
 	public class PrintPitWithStones {
 		
@@ -72,7 +86,6 @@ public class DrawPanel extends JPanel {
 			g2.fill(stone4);
 		}
 		
-		//use in case the user chooses 3 stones
 		public void removeStone4() {
 			g2.setColor(getBackground());
 			g2.fill(stone4);
@@ -98,6 +111,14 @@ public class DrawPanel extends JPanel {
 			g2.fill(stone1);
 			g2.setColor(Color.black);
 
+		}
+		
+		//change each pit from 4 stones to 3 stones
+		public void removePitStonesForThree() {
+			for(PrintPitWithStones p: allPits) {
+				p.removeStone4();
+				p.stone4 = null;
+			}
 		}
 		
 		
@@ -133,43 +154,96 @@ public class DrawPanel extends JPanel {
 		private Ellipse2D.Double stone22;
 		private Ellipse2D.Double stone23;
 		private Ellipse2D.Double stone24;
+		private Ellipse2D.Double stone25;
+		private Ellipse2D.Double stone26;
+		private Ellipse2D.Double stone27;
+		private Ellipse2D.Double stone28;
+		private Ellipse2D.Double stone29;
+		private Ellipse2D.Double stone30;
+		private Ellipse2D.Double stone31;
+		private Ellipse2D.Double stone32;
+		private Ellipse2D.Double stone33;
+		private Ellipse2D.Double stone34;
+		private Ellipse2D.Double stone35;
+		private Ellipse2D.Double stone36;
+		private Ellipse2D.Double stone37;
+		private Ellipse2D.Double stone38;
+		private Ellipse2D.Double stone39;
+		private Ellipse2D.Double stone40;
+		private Ellipse2D.Double stone41;
+		private Ellipse2D.Double stone42;
+		private Ellipse2D.Double stone43;
+		private Ellipse2D.Double stone44;
+		private Ellipse2D.Double stone45;
+		private Ellipse2D.Double stone46;
+		private Ellipse2D.Double stone47;
+		private Ellipse2D.Double stone48;
 
 		
 		
 		public PrintMancalaWithStones(int x, int y, int width, int height) {
 			mancala = new Rectangle2D.Double(x, y, width, height);
 			
-			stone1 = new Ellipse2D.Double(x + 10, y + 22 , 20, 20);
-			stone2 = new Ellipse2D.Double(x + 40, y + 22, 20, 20);
-			stone3 = new Ellipse2D.Double(x + 70, y + 22, 20, 20);
+			stone1 = new Ellipse2D.Double(x + 2, y + 5, width/5, width/5);
+			stone2 = new Ellipse2D.Double(x+28, y+5, width/5, width/5);
+			stone3 = new Ellipse2D.Double(x+53, y+5, width/5, width/5);
+			stone4 = new Ellipse2D.Double(x+78, y+5, width/5, width/5);
 			
-			stone4 = new Ellipse2D.Double(x + 10, y + 57 , 20, 20);
-			stone5 = new Ellipse2D.Double(x + 40, y + 57, 20, 20);
-			stone6 = new Ellipse2D.Double(x + 70, y + 57, 20, 20);
+			stone5 = new Ellipse2D.Double(x + 2, y + 30, width/5, width/5);
+			stone6 = new Ellipse2D.Double(x+28, y+30, width/5, width/5);
+			stone7 = new Ellipse2D.Double(x+53, y+30, width/5, width/5);
+			stone8 = new Ellipse2D.Double(x+78, y+30, width/5, width/5);
 			
-			stone7 = new Ellipse2D.Double(x + 10, y + 92 , 20, 20);
-			stone8 = new Ellipse2D.Double(x + 40, y + 92, 20, 20);
-			stone9 = new Ellipse2D.Double(x + 70, y + 92, 20, 20);
+			stone9 = new Ellipse2D.Double(x + 2, y + 55, width/5, width/5);
+			stone10 = new Ellipse2D.Double(x+28, y+55, width/5, width/5);
+			stone11 = new Ellipse2D.Double(x+53, y+55, width/5, width/5);
+			stone12 = new Ellipse2D.Double(x+78, y+55, width/5, width/5);
 			
-			stone10 = new Ellipse2D.Double(x + 10, y + 127 , 20, 20);
-			stone11 = new Ellipse2D.Double(x + 40, y + 127, 20, 20);
-			stone12 = new Ellipse2D.Double(x + 70, y + 127, 20, 20);
+			stone13 = new Ellipse2D.Double(x + 2, y + 80, width/5, width/5);
+			stone14 = new Ellipse2D.Double(x+28, y+80, width/5, width/5);
+			stone15 = new Ellipse2D.Double(x+53, y+80, width/5, width/5);
+			stone16 = new Ellipse2D.Double(x+78, y+80, width/5, width/5);
 			
-			stone13 = new Ellipse2D.Double(x + 10, y + 162 , 20, 20);
-			stone14 = new Ellipse2D.Double(x + 40, y + 162, 20, 20);
-			stone15 = new Ellipse2D.Double(x + 70, y + 162, 20, 20);
+			stone17 = new Ellipse2D.Double(x + 2, y + 105, width/5, width/5);
+			stone18 = new Ellipse2D.Double(x+28, y+105, width/5, width/5);
+			stone19 = new Ellipse2D.Double(x+53, y+105, width/5, width/5);
+			stone20 = new Ellipse2D.Double(x+78, y+105, width/5, width/5);
 			
-			stone16 = new Ellipse2D.Double(x + 10, y + 197 , 20, 20);
-			stone17 = new Ellipse2D.Double(x + 40, y + 197, 20, 20);
-			stone18 = new Ellipse2D.Double(x + 70, y + 197, 20, 20);
-
-			stone19 = new Ellipse2D.Double(x + 10, y + 232 , 20, 20);
-			stone20 = new Ellipse2D.Double(x + 40, y + 232, 20, 20);
-			stone21 = new Ellipse2D.Double(x + 70, y + 232, 20, 20);
+			stone21 = new Ellipse2D.Double(x + 2, y + 130, width/5, width/5);
+			stone22 = new Ellipse2D.Double(x+28, y+130, width/5, width/5);
+			stone23 = new Ellipse2D.Double(x+53, y+130, width/5, width/5);
+			stone24 = new Ellipse2D.Double(x+78, y+130, width/5, width/5);
 			
-			stone22 = new Ellipse2D.Double(x + 10, y + 267 , 20, 20);
-			stone23 = new Ellipse2D.Double(x + 40, y + 267, 20, 20);
-			stone24 = new Ellipse2D.Double(x + 70, y + 267, 20, 20);
+			stone25 = new Ellipse2D.Double(x + 2, y + 155, width/5, width/5);
+			stone26 = new Ellipse2D.Double(x+28, y+155, width/5, width/5);
+			stone27 = new Ellipse2D.Double(x+53, y+155, width/5, width/5);
+			stone28 = new Ellipse2D.Double(x+78, y+155, width/5, width/5);
+			
+			stone29 = new Ellipse2D.Double(x + 2, y + 180, width/5, width/5);
+			stone30 = new Ellipse2D.Double(x+28, y+180, width/5, width/5);
+			stone31 = new Ellipse2D.Double(x+53, y+180, width/5, width/5);
+			stone32 = new Ellipse2D.Double(x+78, y+180, width/5, width/5);
+			
+			stone33 = new Ellipse2D.Double(x + 2, y + 205, width/5, width/5);
+			stone34 = new Ellipse2D.Double(x+28, y+205, width/5, width/5);
+			stone35 = new Ellipse2D.Double(x+53, y+205, width/5, width/5);
+			stone36 = new Ellipse2D.Double(x+78, y+205, width/5, width/5);
+			
+			stone37 = new Ellipse2D.Double(x + 2, y + 230, width/5, width/5);
+			stone38 = new Ellipse2D.Double(x+28, y+230, width/5, width/5);
+			stone39 = new Ellipse2D.Double(x+53, y+230, width/5, width/5);
+			stone40 = new Ellipse2D.Double(x+78, y+230, width/5, width/5);
+			
+			stone41 = new Ellipse2D.Double(x + 2, y + 255, width/5, width/5);
+			stone42 = new Ellipse2D.Double(x+28, y+255, width/5, width/5);
+			stone43 = new Ellipse2D.Double(x+53, y+255, width/5, width/5);
+			stone44 = new Ellipse2D.Double(x+78, y+255, width/5, width/5);
+			
+			stone45 = new Ellipse2D.Double(x + 2, y + 280, width/5, width/5);
+			stone46 = new Ellipse2D.Double(x+28, y+280, width/5, width/5);
+			stone47 = new Ellipse2D.Double(x+53, y+280, width/5, width/5);
+			stone48 = new Ellipse2D.Double(x+78, y+280, width/5, width/5);
+			
 
 			
 
@@ -182,15 +256,14 @@ public class DrawPanel extends JPanel {
 			g2.fill(stone1);
 			g2.fill(stone2);
 			g2.fill(stone3);
-			
 			g2.fill(stone4);
+			
 			g2.fill(stone5);
 			g2.fill(stone6);
-			
 			g2.fill(stone7);
 			g2.fill(stone8);
-			g2.fill(stone9);
 			
+			g2.fill(stone9);
 			g2.fill(stone10);
 			g2.fill(stone11);
 			g2.fill(stone12);
@@ -198,39 +271,82 @@ public class DrawPanel extends JPanel {
 			g2.fill(stone13);
 			g2.fill(stone14);
 			g2.fill(stone15);
-			
 			g2.fill(stone16);
+			
 			g2.fill(stone17);
 			g2.fill(stone18);
-			
 			g2.fill(stone19);
 			g2.fill(stone20);
-			g2.fill(stone21);
 			
+			g2.fill(stone21);
 			g2.fill(stone22);
 			g2.fill(stone23);
 			g2.fill(stone24);
+			
+			g2.fill(stone25);
+			g2.fill(stone26);
+			g2.fill(stone27);
+			g2.fill(stone28);
+			
+			g2.fill(stone29);
+			g2.fill(stone30);
+			g2.fill(stone31);
+			g2.fill(stone32);
+			
+			g2.fill(stone33);
+			g2.fill(stone34);
+			g2.fill(stone35);
+			g2.fill(stone36);
+			
+			g2.fill(stone37);
+			g2.fill(stone38);
+			g2.fill(stone39);
+			g2.fill(stone40);
+			
+			g2.fill(stone41);
+			g2.fill(stone42);
+			g2.fill(stone43);
+			g2.fill(stone44);
+			
+			g2.fill(stone45);
+			g2.fill(stone46);
+			g2.fill(stone47);
+			g2.fill(stone48);
 			
 			g2.setColor(Color.black);
 			
 			
 		}
 		
-		public void removeStonesForThree(Color backgroundColor) {
-			g2.setColor(backgroundColor);
-			g2.fill(stone22);
-			g2.fill(stone23);
-			g2.fill(stone24);
+		
+		//reduce the number of stones in the mancalca from 48 to 36
+		public void removeMancalaStonesForThree() {
+			stone33 = null;
+			stone34 = null; 
+			stone35 = null;
+			stone36 = null;
 			
-			g2.fill(stone1);
-			g2.fill(stone2);
-			g2.fill(stone3);
+			stone37 = null;
+			stone38 = null; 
+			stone39 = null;
+			stone40 = null;
 			
-			g2.setColor(Color.black);
+			stone41 = null;
+			stone42 = null; 
+			stone43 = null;
+			stone44 = null;
 			
+			stone45 = null;
+			stone46 = null; 
+			stone43 = null;
+			stone44 = null;
 
 		}
 		
-	}
+		public void removePitStonesForThree() {
+			stone1 = null;
+		}
+		
+	}//end of PrintMancalaWithStones
 	
-}//end of PrintMancalaWithStones
+} //end of DrawPanel
--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -1,9 +1,12 @@
+import java.awt.event.*;
+import java.util.*;
 public class MancalaModel{
     private int[] playerAPits;
     private int[] playerBPits;
     private int mancalaA;
     private int mancalaB;
     boolean playerAturn;
+    private ArrayList<ActionListener> listeners;
 
     public MancalaModel(int stones){
         playerAPits = new int[7];
@@ -15,6 +18,174 @@ public class MancalaModel{
         mancalaA = 0;
         mancalaB = 0;
         playerAturn = true;    
+        listeners = new ArrayList<ActionListener>();
+    }
+
+    public void addActionListener(ActionListener listener){
+        listeners.add(listener);
+    }
+
+    public int[] getPlayerAPits(){
+        return playerAPits;
+    }
+    
+    public int[] getPlayerBPits(){
+        return playerBPits;
+    }
+
+    public int getMancalaA(){
+        return mancalaA;
+    }
+
+    public int getMancalaB(){
+        return mancalaB;
+    }
+
+    // This method needs to be called in the controller to check whether if the buttons A[1] --> A[6] are clicked and isPlayerATurn == true.
+    public boolean isPlayerAturn(){
+        return playerAturn;
+    }
+
+    // User A clicks on the empty pit, the controller must do nothing. if(model.isPitEmpty(#) == false) return;
+    public boolean isPitEmpty(int index){
+        if(playerAturn){
+            if(playerAPits[index] == 0)
+                return true;
+        }
+        else{
+            if(playerBPits[index] == 0)
+                return true;
+        }
+        return false;
+    }
+
+    public void mutatePlayerAPits(int index){
+        int stones = playerAPits[index];
+        playerAPits[index] = 0;
+        // for(int i = index + 1; i <= 7; i++){
+        //     if(i == 7 && stones != 0){
+        //         mancalaA++;
+        //         stones--;
+        //         break;
+        //     }
+        //     if(stones == 0){
+        //         if(i == 7){
+        //             playerAturn = true;
+        //         }
+        //         else{
+        //             if(playerAPits[i] == 1 && playerBPits[i] != 0){
+        //                 mancalaA += playerBPits[i] + playerAPits[i];
+        //                 playerBPits[i] = 0;
+        //                 playerAPits[i] = 0;
+        //             }
+        //             playerAturn = false;
+        //         }
+        //         break;
+        //     }
+        //     playerAPits[i]++;
+        //     stones--;
+        // }
+        int i = index + 1;
+        while(stones > 0){
+            for (int j = i; j <= 7; j++){
+                if(j == 7 && stones != 0){
+                    mancalaA++;
+                    stones--;
+                    break;
+                }
+                if(stones == 0){
+                    if(j < 7){
+                        if(playerAPits[j] == 1 && playerBPits[j] != 0){
+                            mancalaA += playerBPits[j] + playerAPits[j];
+                            playerBPits[j] = 0;
+                            playerAPits[j] = 0;
+                        }
+                    }
+                    playerAturn = false;
+                    break;
+                }
+                playerAPits[j]++;
+                stones--;
+            }
+            i = 1;
+            for(int j = i; j < 6; j++){
+                if(stones == 0)
+                    break;
+                playerBPits[j]++;
+                stones--;
+            }
+            i = 1;
+        }
+    }
+
+    public void mutatePlayerBPits(int index){
+        int stones = playerBPits[index];
+        playerBPits[index] = 0;
+        int i = index + 1;
+        while(stones > 0){
+            for (int j = i; j <= 7; j++){
+                if(j == 7 && stones != 0){
+                    mancalaB++;
+                    stones--;
+                    break;
+                }
+                if(stones == 0){
+                    if(j < 7){
+                        if(playerBPits[j] == 1 && playerAPits[j] != 0){
+                            mancalaB += playerBPits[j] + playerAPits[j];
+                            playerBPits[j] = 0;
+                            playerAPits[j] = 0;
+                        }
+                    }
+                    playerAturn = true;
+                    break;
+                }
+                playerBPits[j]++;
+                stones--;
+            }
+            i = 1;
+            for(int j = i; j < 6; j++){
+                if(stones == 0)
+                    break;
+                playerAPits[j]++;
+                stones--;
+            }
+            i = 1;
+        }
+
+        for(ActionListener listener : listeners){
+            listener.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "updatePits"));
+        }
+    }
+
+    public boolean checkEndGame(){
+        if(isEmptyPlayerAPits() || isEmptyPlayerBPits())
+            return true;
+        return false;
+    }
+
+    public boolean isEmptyPlayerAPits(){
+        for(int i = 1; i < 7; i++){
+            if(playerAPits[i] != 0)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isEmptyPlayerBPits(){
+        for(int i = 1; i < 7; i++){
+            if(playerBPits[i] != 0)
+                return false;
+        }
+        return true;
+    }
+
+    public String checkWinner(){
+        if(mancalaA > mancalaB)
+            return "Player A wins!";
+        if(mancalaA < mancalaB)
+            return "Player B wins!";
+        return "It's a tie!";
     }
 
     public void print(){
@@ -32,10 +203,33 @@ public class MancalaModel{
         System.out.println("Mancala B: " + mancalaB);
     }
 
+    public void testEmptyAndWin(){
+        for (int i = 1; i < 7; i++){
+            playerAPits[i] = 0;
+            playerBPits[i] = 0;
+        }
+    }
+
 
     public static void main(String[] args) {
         MancalaModel model = new MancalaModel(4);
         model.print();
-
+        //System.out.println("" + model.isEmptyPlayerAPits() + " " + model.isEmptyPlayerBPits() + " " + model.checkEndGame());
+        if(model.isPlayerAturn()) {
+            if(model.isPitEmpty(2))
+                return;
+            model.mutatePlayerAPits(1);
+            model.print();
+            model.mutatePlayerAPits(2);
+            model.print();
+            model.mutatePlayerAPits(6);
+            model.print();
+        } 
+        // if(!model.isPlayerAturn()) {
+        //     if(model.isPitEmpty(1))
+        //         return;
+        //     //model.mutatePlayerBPits(1);
+        //     model.print();
+        // }
     }
 }
